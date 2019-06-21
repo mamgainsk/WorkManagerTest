34e92c7f9152e4c6d4f41ffedfa4e77584fc5971
Shrikant <shrikantmamgain123@gmail.com>
2019-06-21 19:29:32 +0530

    Initial setup

--- app/src/main/java/com/org/workmanagertest/MainActivity.java
+++ app/src/main/java/com/org/workmanagertest/MainActivity.java
@@ -1,13 +1,40 @@
 package com.org.workmanagertest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userName;
+    private EditText password;
+    private Button logIn;
+    final WorkManager manager = WorkManager.getInstance();
+    final OneTimeWorkRequest workRequest = new OneTimeWorkRequest.Builder(NotificationWorker.class).build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userName = findViewById(R.id.userName);
+        password = findViewById(R.id.password);
+        logIn = findViewById(R.id.logInBtn);
+
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user_name = userName.getText().toString();
+                String user_password = password.getText().toString();
+                manager.enqueue(workRequest);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/org/workmanagertest/NotificationWorker.java
@@ -0,0 +1,12 @@
+package com.org.workmanagertest;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+
+class NotificationWorker extends Worker {
+    @NonNull
+    @Override
+    public WorkerResult doWork() {
+        return null;
+    }
+}
